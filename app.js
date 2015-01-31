3c48e640c075cb501f089a9cdb73a78282614937
Stephen Done <srdone@gmail.com>
2015-01-31 15:45:13 -0700

    Added persistence

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 var app = angular.module('requirementsApp', []);
 
 app.controller('newReqCtrl', function ($scope) {
-  
+
+  var fb = new Firebase('https://fc-requirements.firebaseio.com/');
   var requirements = [];
   var currentPrereqs = [];
+  
+  (function init () {
+    var convertToArray = function (obj) {
+      var arr = [];
+      for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          arr.push(obj[key]);
+        }
+      }
+    };
+  
+    fb.once('value', function (snapshot) {
+      if(snapshot.val()) {
+        requirements = convertToArray(snapshot)
+      } else {
+        requirements = [];
+      };
+    });
+  }());
   
   var clearForm = function () {
     $scope.newRequirement.desc = null;
@@ -24,8 +44,22 @@ app.controller('newReqCtrl', function ($scope) {
     for (var i = 0; i < currentPrereqs.length; i++) {
       req.addPrereq(currentPrereqs[i]); 
     }
-    requirements.push(req);
-    clearForm();
+    var seed = {};
+    seed[req.id] = req;
+    fb.set(requirements.reduce(function (previous, current) {
+      previous[current.id] = current;
+      return previous;  
+    }, seed), function(e) {
+      if (e) {
+        alert('Failed to save. See error log.')
+        console.log(e);
+      } else {
+        console.log('successfully added ' + req);
+        //only push onto requirements array if we were successful in posting it
+        requirements.push(req);
+        clearForm();
+      }
+    });
   };
   
   var addNewPrereq = function (req) {

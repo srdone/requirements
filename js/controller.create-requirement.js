0d8cf4414d3f8b3948ac11cdd2ff4a0b4e34a494
Stephen Done <srdone@gmail.com>
2015-01-31 19:56:33 -0700

    added properties to CreateRequirementController

--- js/controller.create-requirement.js
+++ js/controller.create-requirement.js
@@ -4,5 +4,10 @@ app.controller('CreateRequirementController', function ($scope) {
   
   $scope.newRequirement = {};
   $scope.newRequirement.desc = '';
+  $scope.newRequirement.itemNum = '';
+  $scope.newRequirement.order = 0;
+  $scope.newRequirement.award = '';
+  $scope.newRequirement.isAward = false;
+  $scope.newRequirement.type = '';
   
 });
--- test/test.controller.create-requirement.js
+++ test/test.controller.create-requirement.js
@@ -13,7 +13,34 @@ describe('CreateRequirementController', function() {
     
   }));
   
-  it('should have a newRequirement.desc property', function() {
+  it('should have a newRequirement.desc property, type string, empty', function() {
     expect(scope.newRequirement.desc).toBeDefined();
+    expect(scope.newRequirement.desc).toBeEmptyString();
+  });
+  
+  it('should have a newRequirement.itemNum property, type string, empty', function () {
+    expect(scope.newRequirement.itemNum).toBeDefined();
+    expect(scope.newRequirement.itemNum).toBeEmptyString();
+  });
+  
+  it('should have a newRequirement.order property, type integer, 0', function () {
+    expect(scope.newRequirement.order).toBeDefined();
+    expect(scope.newRequirement.order).toBeWholeNumber();
+    expect(scope.newRequirement.order).toBe(0);
+  });
+  
+  it('should have a newRequirement.award property, type string, empty', function() {
+    expect(scope.newRequirement.award).toBeDefined();
+    expect(scope.newRequirement.award).toBeEmptyString();
+  });
+  
+  it('should have a newRequirement.isAward property, type boolean, false', function () {
+    expect(scope.newRequirement.isAward).toBeDefined();
+    expect(scope.newRequirement.isAward).toBeFalse();
+  });
+  
+  it('should have a newRequirement.type property, type string, empty', function () {
+    expect(scope.newRequirement.type).toBeDefined();
+    expect(scope.newRequirement.type).toBeEmptyString();
   });
 });
